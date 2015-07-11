34ffd3932b8042b13ae3931ae8a6cb28a572ce45
jianye.liu <jianye.liu@alipay.com>
2015-07-11 14:48:37 +0800

    add server.js

---
+++ server/server.js
@@ -0,0 +1,6 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '47.88.13.171');
+console.log('Server running at http://127.0.0.1:1337/');
